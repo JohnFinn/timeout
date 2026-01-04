cde93126e525fe3f7f4bcb832dab0d7465c4a4a1
JohnFinn <dz4tune@gmail.com>
2026-01-04 22:56:34 +0100

    speedup

--- test.cpp
+++ test.cpp
@@ -1,13 +1,33 @@
 #include "timeout.hpp"
-#include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
 #include <thread>
 
-TEST(Foo, Bar) {
+TEST(Timeout, Smoke) {
   using namespace std::chrono_literals;
-  EXPECT_EQ(timeout(10ms, [](){ return 42; }), 42);
-  EXPECT_EQ(timeout(10ms, [](){ std::this_thread::sleep_for(11ms); return 42; }), std::nullopt);
+  EXPECT_EQ(timeout(10ms, []() { return 42; }), 42);
+  EXPECT_EQ(timeout(10ms,
+                    []() {
+                      std::this_thread::sleep_for(11ms);
+                      return 42;
+                    }),
+            std::nullopt);
   int a = 10;
-  EXPECT_EQ(timeout(10ms, [&](){ return a + a; }), 20);
-  EXPECT_EQ(timeout(10ms, [](int a, int b){ return a + b; }, 42, 54), 96);
+  EXPECT_EQ(timeout(10ms, [&]() { return a + a; }), 20);
+  EXPECT_EQ(timeout(10ms, [](int a, int b) { return a + b; }, 42, 54), 96);
+}
+
+TEST(Timeout, Threads) {
+  auto f = [](int a, int b) { return a + b; };
+  using namespace std::chrono_literals;
+  std::jthread t([&]() {
+    for (int i = 0; i < 100; ++i) {
+      EXPECT_EQ(timeout(10ms, f, 1, 2), 3);
+    }
+  });
+  std::jthread t2([&]() {
+    for (int i = 0; i < 100; ++i) {
+      EXPECT_EQ(timeout(10ms, f, 2, 2), 4);
+    }
+  });
 }
--- timeout.hpp
+++ timeout.hpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <iostream>
 #include <optional>
 #include <poll.h>
 #include <signal.h>
@@ -16,16 +17,18 @@ namespace {
 void throw_errno() { throw std::runtime_error(strerror(errno)); }
 
 class SubprocessHandle {
-  pid_t _pid;
+  std::optional<pid_t> _pid;
+
+  explicit SubprocessHandle() : _pid(std::nullopt) {}
 
 public:
   explicit SubprocessHandle(pid_t pid) : _pid(pid) {}
 
-  static std::optional<SubprocessHandle> fork() {
+  static SubprocessHandle fork() {
     if (int pid = ::fork(); pid > 0) {
-      return std::optional<SubprocessHandle>(pid);
+      return SubprocessHandle(pid);
     } else if (pid == 0) {
-      return std::nullopt;
+      return SubprocessHandle();
     } else {
       throw_errno();
     }
@@ -33,29 +36,45 @@ public:
 
   SubprocessHandle(const SubprocessHandle &) = delete;
   SubprocessHandle &operator=(const SubprocessHandle &) = delete;
-  SubprocessHandle(SubprocessHandle &&) = delete;
-  SubprocessHandle &operator=(SubprocessHandle &&) = delete;
+  SubprocessHandle(SubprocessHandle &&other)
+      : _pid(std::exchange(other._pid, std::nullopt)) {}
+  SubprocessHandle &operator=(SubprocessHandle &&other) {
+    _pid = std::exchange(other._pid, std::nullopt);
+    return *this;
+  }
 
-  void kill() { ::kill(_pid, SIGKILL); }
+  void kill() { ::kill(_pid.value(), SIGKILL); }
 
-  ~SubprocessHandle() { ::waitpid(_pid, nullptr, 0); }
+  operator bool() const { return _pid.has_value(); }
+
+  ~SubprocessHandle() {
+    if (_pid)
+      ::waitpid(*_pid, nullptr, 0);
+  }
 };
 
 struct pipe_fds {
 private:
-  int _fds[2];
+  struct fds {
+    int _fds[2];
+  };
 
-  int read_fd() const { return _fds[0]; }
-  int write_fd() const { return _fds[1]; }
+  std::optional<fds> _fds;
+
+  int read_fd() const { return _fds->_fds[0]; }
+  int write_fd() const { return _fds->_fds[1]; }
 
 public:
   pipe_fds(const pipe_fds &) = delete;
   pipe_fds &operator=(const pipe_fds &) = delete;
-  pipe_fds(pipe_fds &&) = delete;
-  pipe_fds &operator=(pipe_fds &&) = delete;
+  pipe_fds(pipe_fds &&other) : _fds(std::exchange(other._fds, std::nullopt)) {}
+  pipe_fds &operator=(pipe_fds &&other) {
+    _fds = std::exchange(other._fds, std::nullopt);
+    return *this;
+  }
 
-  pipe_fds() {
-    if (pipe(_fds) != 0) {
+  pipe_fds() : _fds(fds()) {
+    if (pipe(_fds->_fds) != 0) {
       throw_errno();
     }
   }
@@ -160,26 +179,45 @@ public:
   }
 };
 
+template <typename F, typename... Args> class Worker {
+  pipe_fds retval_pipe;
+  pipe_fds args_pipe;
+  using tuple_t = std::tuple<Args...>;
+  using res_t = std::invoke_result_t<F, Args...>;
+  SubprocessHandle subprocess;
+  F f;
+
+public:
+  Worker(F f) : f(std::move(f)), subprocess(SubprocessHandle::fork()) {
+    if (!subprocess) {
+      while (true) {
+        retval_pipe.serialize(
+            std::apply(f, args_pipe.deserialize_tuple<Args...>()));
+      }
+    }
+  }
+  Worker(Worker &&) = default;
+  Worker &operator=(Worker &&) = default;
+
+  std::optional<res_t> operator()(std::chrono::milliseconds duration,
+                                  const Args &...args) {
+    args_pipe.serialize_tuple(tuple_t(args...));
+    if (retval_pipe.poll_read(duration)) {
+      return retval_pipe.deserialize<res_t>();
+    }
+    subprocess.kill();
+    return std::nullopt;
+  }
+
+  ~Worker() { subprocess.kill(); }
+};
+
 } // namespace
 
 template <typename F, typename... Args>
   requires std::is_trivially_copyable_v<std::invoke_result_t<F, Args...>>
 const std::optional<std::invoke_result_t<F, Args...>>
 timeout(std::chrono::milliseconds duration, F f, const Args &...args) {
-  pipe_fds retval_pipe;
-  pipe_fds args_pipe;
-  using tuple_t = std::tuple<Args...>;
-  using res_t = std::invoke_result_t<F, Args...>;
-  if (auto subprocess = SubprocessHandle::fork(); subprocess.has_value()) {
-    args_pipe.serialize_tuple(tuple_t(args...));
-    if (retval_pipe.poll_read(duration)) {
-      return retval_pipe.deserialize<res_t>();
-    }
-    subprocess->kill();
-    return std::nullopt;
-  } else {
-    retval_pipe.serialize(
-        std::apply(f, args_pipe.deserialize_tuple<Args...>()));
-    std::exit(0);
-  }
+  static thread_local Worker<F, Args...> worker(f);
+  return worker(duration, args...);
 };
